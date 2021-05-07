4b17f3764e06803199a076ed05ca74e632cfa40f
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-05-07 12:45:14 -0400

    notifier module

---
+++ bot/notifier.py
@@ -0,0 +1,5 @@
+def buyOrder(coin, price, amount):
+    print('The Bot is buying',amount,'of',coin,'at',price)
+    
+def sellOrder(coin, price, amount):
+    print('The Bot is selling',amount,'of',coin,'at',str(price)+'.','Your profit is')

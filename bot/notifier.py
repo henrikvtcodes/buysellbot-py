6bf02a60f44ce32d9f86d4555e9c199cd3972fc2
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-05-08 16:26:48 -0400

    notifier v1.1: processNotif function created

--- bot/notifier.py
+++ bot/notifier.py
@@ -10,5 +10,10 @@ def buyOrder(coin, price, amount):
 def sellOrder(coin, price, amount):
     bc=r.baseConfig()
     notif=bc.get('notifications')
+    
+    msg=('The Bot is selling',amount,'of',coin,'at',str(price)+'.','Your profit is')
     if notif.get('sell') == True:
-        print('The Bot is selling',amount,'of',coin,'at',str(price)+'.','Your profit is')
+        print(msg)
+        
+def processNotif(threadNo, event):
+    msg=('Thread No.',str(threadNo)+':','has been',event+'.')

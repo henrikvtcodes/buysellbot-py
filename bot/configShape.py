dcf46494005f84ca4f0a9a466ed6d8661a5e0882
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-05-08 14:43:04 -0400

    add new data

--- bot/configShape.py
+++ bot/configShape.py
@@ -1,3 +1,5 @@
+# THIS SCRIPT IS NOT USED IN OPERATION OF THIS PROGRAM
+
 {
 'baseConfig': 
     [
@@ -29,3 +31,25 @@
         ]
     }
 }
+
+
+dict_items([
+    ('baseConfig', [
+      {'currency': 'USD'}, 
+      {'notifications': [
+          {'checking': True}, 
+          {'buy': True}, 
+          {'sell': True}, 
+          {'error': True}, 
+          {'misc': True}
+          ]
+       }, 
+      {'maxHold': 100}, 
+      {'maxBuy': 20}, 
+      {'maxSell': None}
+      ]
+     )
+    ]
+)
+
+dict_items([('coins', {'example': [{'announceName': 'Bitcoin'}, {'coin': 'BTC'}, {'maxHold': 69}, {'buy': True}, {'buyAt': 42000}, {'buyAmt': 10}, {'sell': True}, {'sellAtHigh': 69000}, {'sellAmtHigh': '15%'}, {'sellAtLow': 40000}, {'sellAmtLow': '100%'}]})])

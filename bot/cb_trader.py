c13be108595c1d61c01bb457f984c13d7d0b4943
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-05-09 12:08:20 -0400

    add try/catch block to login code | create price checker method

--- bot/cb_trader.py
+++ bot/cb_trader.py
@@ -1,9 +1,13 @@
 from envs import env
-import json
 
 from coinbase.wallet.client import Client
 
-global client
+import ryaml as r
+
+baseConf = r.baseConfig()
+coinConf = r.coinConfig()
+cur = baseConf[0]
+fiat = cur.get('currency')
 
 try:
     key = env('COINBASE_KEY')
@@ -11,11 +15,41 @@ try:
 except:
     print("API Key Not Found.")
 else:
-    client = Client(key, secret)
+    try:
+        client = Client(key, secret)
+    except:
+        print('API Key Error')
+        print('Invalid Key:',key)
+        print('Invalid Secret:',secret)
 
-def getCurrentUserData(returnType):
+def getCurrentUserData(returnType): #get the properties of the user from their API Key
     userObj = json.loads(client.get_current_user())
-    print(userObj)
 
-# def buy(coin, amount):
+def getPrice(crypto):
+    priceObj = (str(crypto)+'-'+str(fiat))
     
+    try:
+        spotPrice = json.loads(client.get_spot_price(currency_pair = priceObj))
+    except:
+        spotPrice = 'OP_FAILED'
+    
+    try:
+        buyPrice = json.loads(client.get_buy_price(currency_pair = priceObj))
+    except:
+        spotPrice = 'OP_FAILED'
+    
+    try:
+        sellPrice = json.loads(client.get_sell_price(currency_pair = priceObj))
+    except:
+        spotPrice = 'OP_FAILED'
+    
+    priceList = list()
+    priceList.append(spotPrice)
+    priceList.append(buyPrice)
+    priceList.append(sellPrice)
+    return priceList
+
+# def validateOrder(isBuy, orderValue):#make sure that an buy or sell order is within parameters
+    
+    
+# def buy(coin, amount):
